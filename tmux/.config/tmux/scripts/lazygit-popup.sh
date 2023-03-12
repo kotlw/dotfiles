8b44d75d788be1009ea8a00f543f3f4f0efbc1ec
vk <kotliarvladyslav@gmail.com>
2023-03-12 19:53:47 +0200

    fix tmux lazygit-popup script

--- tmux/.config/tmux/scripts/lazygit-popup.sh
+++ tmux/.config/tmux/scripts/lazygit-popup.sh
@@ -2,12 +2,14 @@
 source ~/.zprofile
 repo_root="$1"
 
-while [[ ! -d "${repo_root}/.git" && "$(dirname "${repo_root}")" != "${repo_root}" ]]; do
+while [[ ! -e "${repo_root}/.git" && "$(dirname "${repo_root}")" != "${repo_root}" ]]; do
   repo_root="$(dirname "${repo_root}")"
 done
 
 if [[ "${repo_root}" == "/" ]]; then
   tmux display-message "unable to find .git folder"
-else
+elif [[ -d "${repo_root}/.git" ]]; then
   tmux popup -E -w 95% -h 95% "exec $SHELL -lc 'lazygit -p ${repo_root}'"
+elif [[ -f "${repo_root}/.git" ]]; then
+  tmux popup -E -w 95% -h 95% "exec $SHELL -lc 'lazygit -w ${repo_root}'"
 fi
